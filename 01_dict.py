cba4fffe4e16fc8eb0c8415e03144af105cb454c
Aakanshu Tomar <128179471+tomar96@users.noreply.github.com>
2023-12-31 13:51:46 +0530

    Add files via upload

---
+++ 01_dict.py
@@ -0,0 +1,9 @@
+first={
+    "fast":"in a quick manner.",
+    "jaat":"a power brand.",
+    "another" : {'aakams':'player'} #we can print a dic in a dic also
+    
+}
+first["fast"]=[45,78]
+print(first["fast"])
+print(first["another"])
